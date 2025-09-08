9c8aa09373f2039647e4e245293a679993153d90
Gustavo Silvestre <g.silv.bits@gmail.com>
2025-09-08 13:36:02 -0300

    atualização

---
+++ Exercicios por conta propria/Exercicios JavaScript com HTML e CSS/desafio001/d001.js
@@ -0,0 +1,11 @@
+
+function Mensagem1(){
+    return alert('Você apertou o botão 1')
+}
+
+function Mensagem2(){
+    alert('Você apertou o botão 2')
+}
+function Mensagem3(){
+    alert('Você apertou o botão 3')
+}
---
+++ Exercicios por conta propria/Exercicios JavaScript com HTML e CSS/desafio002/d002.js
@@ -0,0 +1,8 @@
+
+
+function Mensagem1(){
+    let nome = prompt('Olá, qual o seu nome?');
+    let idade = prompt('Olá, qual a sua idade?');
+    alert(`Acabei de conhecer ${nome}, que tem ${idade} anos!`)
+    
+}
