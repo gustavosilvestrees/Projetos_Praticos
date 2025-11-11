e9a531a982b7f71f046d1a561e978021497de068
Gustavo Silvestre <g.silv.bits@gmail.com>
2025-11-11 17:46:01 -0300

    atualização

---
+++ Projeto 4 Pagina Portfolio/script.js
@@ -0,0 +1,6 @@
+/* Este arquivo está conectado ao index.html, pronto para o seu código JavaScript! */
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Sistema Iniciado: Portfólio carregado com sucesso!');
+    // Seu código JavaScript virá aqui!
+});
