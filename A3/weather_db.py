feb27176a0246dce9eb4b0e1b29a01fd4e4c7436
Gustavo Silvestre <g.silv.bits@gmail.com>
2026-05-27 17:07:32 -0300

    adição de projeto A3

---
+++ A3/weather_db.py
@@ -0,0 +1,33 @@
+# weather_db.py
+
+# Banco de dados simulando uma coleção de objetos estruturados
+IMAGENS_ESTADOS = {
+    0: {"nome": "Genérico", "imagem": "imagens/nacional/generico.jpg"},
+    1: {"nome": "Acre", "imagem": "imagens/nacional/acre.jpg"},
+    2: {"nome": "Alagoas", "imagem": "imagens/nacional/alagoas.jpg"},
+    3: {"nome": "Amapá", "imagem": "imagens/nacional/amapa.jpg"},
+    4: {"nome": "Amazonas", "imagem": "imagens/nacional/amazonas.jpg"},
+    5: {"nome": "Bahia", "imagem": "imagens/nacional/bahia.jpg"},
+    6: {"nome": "Ceará", "imagem": "imagens/nacional/ceara.jpg"},
+    7: {"nome": "Distrito Federal", "imagem": "imagens/nacional/df.jpg"},
+    8: {"nome": "Espírito Santo", "imagem": "imagens/nacional/espiritosanto.jpg"},
+    9: {"nome": "Goiás", "imagem": "imagens/nacional/goias.jpg"},
+    10: {"nome": "Maranhão", "imagem": "imagens/nacional/maranhao.jpg"},
+    11: {"nome": "Mato Grosso", "imagem": "imagens/nacional/matogrosso.jpg"},
+    12: {"nome": "Mato Grosso do Sul", "imagem": "imagens/nacional/matogrossodosul.jpg"},
+    13: {"nome": "Minas Gerais", "imagem": "imagens/nacional/minasgerais.jpg"},
+    14: {"nome": "Pará", "imagem": "imagens/nacional/para.jpg"},
+    15: {"nome": "Paraíba", "imagem": "imagens/nacional/paraiba.jpg"},
+    16: {"nome": "Paraná", "imagem": "imagens/nacional/parana.jpg"},
+    17: {"nome": "Pernambuco", "imagem": "imagens/nacional/pernambuco.jpg"},
+    18: {"nome": "Piauí", "imagem": "imagens/nacional/piaui.jpg"},
+    19: {"nome": "Rio de Janeiro", "imagem": "imagens/nacional/riodejaneiro.jpg"},
+    20: {"nome": "Rio Grande do Norte", "imagem": "imagens/nacional/riograndedonorte.jpg"},
+    21: {"nome": "Rio Grande do Sul", "imagem": "imagens/nacional/riograndedosul.jpg"},
+    22: {"nome": "Rondônia", "imagem": "imagens/nacional/rondonia.jpg"},
+    23: {"nome": "Roraima", "imagem": "imagens/nacional/roraima.jpg"},
+    24: {"nome": "Santa Catarina", "imagem": "imagens/nacional/santacatarina.jpg"},
+    25: {"nome": "São Paulo", "imagem": "imagens/nacional/saopaulo.jpg"},
+    26: {"nome": "Sergipe", "imagem": "imagens/nacional/sergipe.jpg"},
+    27: {"nome": "Tocantins", "imagem": "imagens/nacional/tocantins.jpg"}
+}
